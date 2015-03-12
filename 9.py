3aa5e80c184dbc81696605e0ddddaa4d20fae78a
Kyle Thornburgh <kat4ma@virginia.edu>
2015-03-12 01:40:36 -0400

    Adds solution to 9.

---
+++ 9.py
@@ -0,0 +1,15 @@
+
+
+for a in range(1, 1000):
+
+	for b in range(2, 1000):
+
+		for c in range(3, 1000):
+
+			if a + b + c == 1000:
+
+				if pow(a, 2) + pow(b, 2) == pow(c, 2):
+
+					print(str(a) + " + " + str(b) + " + " + str(c) + " = " + str(a+b+c))
+
+	print(a)
