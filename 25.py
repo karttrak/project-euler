9b232df9f28e173493a39bea5fed4025c68f8fcf
Kyle Thornburgh <kat4ma@virginia.edu>
2015-03-12 01:59:39 -0400

    Adds solution to 25.

---
+++ 25.py
@@ -0,0 +1,22 @@
+
+
+def fib(n):
+
+	a = b = 1
+	j = 1
+	size = len(str(a))
+	print(str(j) + ": " + str(size))
+
+	while size != 1000:
+
+		c = a+b
+		a = b
+		b = c
+
+		size = len(str(a))
+		j += 1
+
+		print(str(j) + ": " + str(size))
+		
+
+fib(100)
